57bf0d325430cf1083731b693788d0185875d949
smartkiwi <smartkiwi@gmail.com>
2011-02-13 20:44:48 -0500

    starting s1a task plus initial environment setup instructions

---
+++ s1a/S1a.java
@@ -0,0 +1,16 @@
+/*
+1.a. Create an interface that has 
+		methods and 
+		a constant. 
+	Create an abstract class with 
+		an abstract method, 
+		a non-abstract method, and 
+		an instance variable. 
+	Create a concrete class that uses both of the above.
+*/
+
+interface S1AInterface {
+	void method1();
+	void method2();
+	int x = 0;
+}
