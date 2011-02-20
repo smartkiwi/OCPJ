f8fa93f2fa2ddd652174cd934905229c93a44ccb
smartkiwi <smartkiwi@gmail.com>
2011-02-19 21:01:33 -0500

    S2e solved

--- s2e/S2e.java
+++ s2e/S2e.java
@@ -1,3 +1,24 @@
 /*
-2.e Write a try-catch-finally and demonstrate it doing a try-catch-finally and then try finally.
+2.e Write a try-catch-finally and 
+	demonstrate it doing a try-catch-finally and 
+	then try finally.
 */
+
+
+class S2e {
+	public static void main(String[] args) {
+		System.out.println("Start");
+		try {
+			System.out.println("we are in try block");
+			int i = 1/0;
+			System.out.println("program will never get here");
+			assert(true);
+		} catch (Exception e) {
+			System.out.println("we caught exception:");
+			e.printStackTrace();			
+		} finally {
+			System.out.println("...and FINALLY...");
+		}
+		System.out.println("ending program gracefully");
+	}
+}
